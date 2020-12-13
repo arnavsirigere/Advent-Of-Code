bb784b3b60afc10ca469c74d3074b0dbcc4e419a
Arnav Sirigere <arnavsirigere@gmail.com>
2020-12-13 22:31:36 +0530

    :recycle: Implemented copy function into BigNumber

--- 2020/Day 13/Part 2.js
+++ 2020/Day 13/Part 2.js
@@ -1,12 +1,20 @@
 const BigNumber = require('big-number');
 
+/*
+Implemented my own copy function into BigNumber - 
+
+BigNumber.prototype.copy = function () {
+    return BigNumber(this.toString());
+};
+*/
+
 function solve(input) {
   const busIds = input[1].split(',');
   const nums = busIds.filter(elt => elt !== 'x').map(BigNumber);
   const rems = busIds.map((elt, index) => (elt !== 'x' ? (index == 0 ? 0 : BigNumber(elt).subtract(index % +elt)) : null)).filter(num => num !== null); // Remainders
   const product = nums.reduce((prod, num) => prod.mult(num), BigNumber(1));
-  const productDivided = nums.map(num => BigNumber(product.toString()).div(num));
-  const multInverse = productDivided.map((num, index) => modInverse(BigNumber(num.toString()), BigNumber(nums[index].toString())));
+  const productDivided = nums.map(num => product.copy().div(num));
+  const multInverse = productDivided.map((num, index) => modInverse(num.copy(), nums[index].copy()));
   return rems
     .map((rem, index) => productDivided[index].mult(multInverse[index]).mult(rem))
     .reduce((sum, num) => sum.add(num), BigNumber(0))
@@ -16,7 +24,7 @@ function solve(input) {
 function modInverse(a, m) {
   a = a.mod(m);
   for (let x = BigNumber(1); x.lt(m); x.add(1)) {
-    if (BigNumber(a.toString()).mult(x).mod(m).equals(1)) {
+    if (a.copy().mult(x).mod(m).equals(1)) {
       return x;
     }
   }
