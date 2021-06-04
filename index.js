45df9d0630012108efd61f0b95831c6de71e5250
Arnav Sirigere <arnavsirigere@gmail.com>
2021-06-03 21:59:54 -0500

    Add support for a puzzle specified by day and year in process arguments

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
-const years = getDirectories('./').filter((dir) => /\d+/.test(dir));
-
-for (let year of years) {
+let [day, year] = process.argv.slice(2, 4);
+if (day && year) {
+  // Specific Puzzle
   console.log(chalk.magenta.underline.bold(`Advent of Code ${year}`));
-  const path = `./${year}/`;
-  const days = getDirectories(path);
-  for (const day of days) {
-    console.log('  ' + chalk.blue.underline(day));
-    const puzzlePath = path + day;
-    const { delimiter } = require(`${puzzlePath}/Part 1`);
-    const input = fs
-      .readFileSync(`${puzzlePath}/input.txt`)
-      .toString()
-      .split(delimiter || '\n');
-    for (let part of [1, 2]) {
-      const solver = require(`${puzzlePath}/Part ${part}`);
-      const answer = solver.solve(input.length == 1 ? input[0] : input);
-      console.log(chalk.yellow(`    Part ${part}`) + ' : ' + chalk.green(answer));
+  printPuzzle(`Day ${day}`, year);
+} else {
+  // All solved puzzles
+  const years = getDirectories('./').filter((dir) => /\d+/.test(dir));
+  for (let year of years) {
+    const days = getDirectories(`./${year}/`);
+    console.log(chalk.magenta.underline.bold(`Advent of Code ${year}`));
+    for (let day of days) {
+      printPuzzle(day, year);
     }
-    console.log('');
   }
+}
+
+function printPuzzle(day, year) {
+  console.log('  ' + chalk.blue.underline(`${day}`));
+  const puzzlePath = `./${year}/${day}`;
+  const { delimiter } = require(`${puzzlePath}/Part 1`);
+  const input = fs
+    .readFileSync(`${puzzlePath}/input.txt`)
+    .toString()
+    .split(delimiter || '\n');
+  for (let part of [1, 2]) {
+    const solver = require(`${puzzlePath}/Part ${part}`);
+    const answer = solver.solve(input.length == 1 ? input[0] : input);
+    console.log(chalk.yellow(`    Part ${part}`) + ' : ' + chalk.green(answer));
+  }
+  console.log('');
 }
 
 function getDirectories(path) {
