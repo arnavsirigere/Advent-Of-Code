3aaeda46bd1c63b56a64b5555a828df588aa60f2
Arnav Sirigere <arnavsirigere@gmail.com>
2022-02-08 21:35:34 +0530

    Uncomment code

--- 2020/Day 13/Part 2.js
+++ 2020/Day 13/Part 2.js
@@ -1,19 +1,15 @@
 const BigNumber = require('big-number');
 
-/*
-Implemented my own copy function into BigNumber - 
-
 BigNumber.prototype.copy = function () {
-    return BigNumber(this.toString());
+  return BigNumber(this.toString());
 };
-*/
 
 function solve(input) {
   const busIds = input[1].split(',');
-  const nums = busIds.filter(elt => elt !== 'x').map(BigNumber);
-  const rems = busIds.map((elt, index) => (elt !== 'x' ? (index == 0 ? 0 : BigNumber(elt).subtract(index % +elt)) : null)).filter(num => num !== null); // Remainders
+  const nums = busIds.filter((elt) => elt !== 'x').map(BigNumber);
+  const rems = busIds.map((elt, index) => (elt !== 'x' ? (index == 0 ? 0 : BigNumber(elt).subtract(index % +elt)) : null)).filter((num) => num !== null); // Remainders
   const product = nums.reduce((prod, num) => prod.mult(num), BigNumber(1));
-  const productDivided = nums.map(num => product.copy().div(num));
+  const productDivided = nums.map((num) => product.copy().div(num));
   const multInverse = productDivided.map((num, index) => modInverse(num.copy(), nums[index].copy()));
   return rems
     .map((rem, index) => productDivided[index].mult(multInverse[index]).mult(rem))
