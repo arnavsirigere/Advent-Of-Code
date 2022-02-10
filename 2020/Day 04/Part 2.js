705f6cfb4b02e9e986131aa84dc979b2c8da0833
Arnav Sirigere <arnavsirigere@gmail.com>
2022-02-10 23:45:36 +0530

    Fix solution

--- 2020/Day 04/Part 2.js
+++ 2020/Day 04/Part 2.js
@@ -11,23 +11,23 @@ function solve(input) {
 }
 
 function validatePassport(passport) {
-  if (!requiredFields.every(fieldName => new RegExp(`${fieldName}:`).test(passport))) return false;
-  const fields = passport.split(' ');
-  return fields.every(field => {
+  if (!requiredFields.every((fieldName) => new RegExp(`${fieldName}:`).test(passport))) return false;
+  const fields = passport.match(/\w+\:\#?\w+/g);
+  return fields.every((field) => {
     const [fieldName, fieldValue] = field.split(':');
     return validator[fieldName](fieldValue);
   });
 }
 
 const validator = {
-  byr: byr => byr.length == 4 && between(+byr, 1920, 2002),
-  iyr: iyr => iyr.length == 4 && between(+iyr, 2010, 2020),
-  eyr: eyr => eyr.length == 4 && between(+eyr, 2020, 2030),
-  hcl: hcl => /\#[a-f0-9]{6}/.test(hcl),
-  ecl: ecl => ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(ecl),
-  pid: pid => /^\d{9}$/.test(pid),
+  byr: (byr) => byr.length == 4 && between(+byr, 1920, 2002),
+  iyr: (iyr) => iyr.length == 4 && between(+iyr, 2010, 2020),
+  eyr: (eyr) => eyr.length == 4 && between(+eyr, 2020, 2030),
+  hcl: (hcl) => /\#[a-f0-9]{6}/.test(hcl),
+  ecl: (ecl) => ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(ecl),
+  pid: (pid) => /^\d{9}$/.test(pid),
   cid: () => true,
-  hgt: hgt => {
+  hgt: (hgt) => {
     if (!/\d{2,3}(in|cm)/.test(hgt)) return false;
     let [_, length, metricType] = hgt.match(/(\d{2,3})((in|cm))/);
     length = +length;
